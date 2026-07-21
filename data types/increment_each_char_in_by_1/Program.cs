d2156add1c5842a4c75fc7d7ac95861494b618eb
Bhanu <kolagani346@gmail.com>
2026-07-21 15:11:46 +0530

    Add files via upload

---
+++ data types/increment_each_char_in_by_1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a string: ");
+        string str = Console.ReadLine();
+
+        string result = "";
+
+        foreach (char ch in str)
+        {
+            char c = (char)(ch + 1);
+
+            if (char.IsLetter(c))
+            {
+                if (char.IsUpper(c))
+                    c = char.ToLower(c);
+                else
+                    c = char.ToUpper(c);
+            }
+
+            result += c;
+        }
+
+        Console.WriteLine("New String: " + result);
+    }
+}
